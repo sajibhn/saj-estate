8e14b725cac73df5b4b34a0003e785b395e514fb
Sajib Hossain <sajibhossain1185@gmail.com>
2022-06-26 22:34:45 +0600

    added filter in listing page

--- components/reusable/ListingCard.jsx
+++ components/reusable/ListingCard.jsx
@@ -1,53 +1,82 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { MdBed } from 'react-icons/md'
 import { FaShower } from 'react-icons/fa'
 import { AiFillCar } from 'react-icons/ai'
 import Link from 'next/link'
 
-const ListingCard = ({ data, simplified }) => {
-    const count = simplified ? 3 : data?.listings?.data?.length
-    return (
-        <div className="listing__card__container">
+const ListingCard = ({ data, simplified, loading }) => {
+    const [listingDatas, setListingData] = useState(data?.listings?.data)
+    const [active, setActive] = useState('')
+    const count = simplified ? 3 : listingDatas?.listings?.data?.length
+    const ForSell = (e) => {
+        const forSell = data?.listings?.data?.filter((item) => item.attributes.type.toLowerCase() === 'for sale')
+        setListingData(forSell)
+        setActive(e.target.id);
+    }
+    const ForRent = (e) => {
+        const forSale = data?.listings?.data?.filter((item) => item.attributes.type.toLowerCase() === 'for rent')
+        setListingData(forSale)
+        setActive(e.target.id);
+    }
 
-            {
-                data?.listings?.data?.slice(0, count).map((item, index) => {
-                    const { title, location, price, description, bedrooms, bathrooms, garage, featured_image } = item.attributes
-                    const fImage = featured_image.data.attributes.url
-                    return (
-                        <>
-                            <Link href={`/property/${index}`}>
-                                <div className="listing__card" key={index}>
-                                    <div className="listing__card__head">
-                                        <img src={`http://localhost:1337${fImage}`} />
-                                    </div>
-                                    <div className="listing__card__body">
-                                        <p>{location}</p>
-                                        <h3>{title}</h3>
-                                        <span className='price'>$ {price}</span>
-                                        <p>{description?.substring(0, 75)} ...</p>
-                                        <div className="listing__card__info">
-                                            <div>
-                                                <span><MdBed /></span>
-                                                <span>{bedrooms}</span>
-                                            </div>
-                                            <div>
-                                                <FaShower />
-                                                <span>{bathrooms}</span>
-                                            </div>
-                                            <div>
-                                                <AiFillCar />
-                                                <span>{garage}</span>
+    const All = (e) => {
+        setListingData(data?.listings?.data)
+        setActive(e.target.id);
+    }
+
+    if (loading) return <p>Loading....</p>
+    return (
+        <>
+            <div className="filter">
+                <h2>Filter</h2>
+                <div className="filter__buttons">
+                    <button key={1} className={active === '1' ? 'filter_active' : undefined} id={'1'} onClick={All}>all</button>
+                    <button key={2} className={active === '2' ? 'filter_active' : undefined} id={'2'} onClick={ForSell}>for sell</button>
+                    <button key={3} className={active === '3' ? 'filter_active' : undefined} id={'3'} onClick={ForRent}>for rent</button>
+                </div>
+            </div>
+            <div className="listing__card__container">
+
+                {
+                    listingDatas?.slice(0, count).map((item, index) => {
+                        const { title, location, price, description, bedrooms, bathrooms, garage, featured_image } = item.attributes
+                        const fImage = featured_image.data.attributes.url
+                        return (
+                            <>
+                                <Link href={`/property/${index}`}>
+                                    <div className="listing__card" key={index}>
+                                        <div className="listing__card__head">
+                                            <img src={`http://localhost:1337${fImage}`} />
+                                        </div>
+                                        <div className="listing__card__body">
+                                            <p>{location}</p>
+                                            <h3>{title}</h3>
+                                            <span className='price'>$ {price}</span>
+                                            <p>{description?.substring(0, 75)} ...</p>
+                                            <div className="listing__card__info">
+                                                <div>
+                                                    <span><MdBed /></span>
+                                                    <span>{bedrooms}</span>
+                                                </div>
+                                                <div>
+                                                    <FaShower />
+                                                    <span>{bathrooms}</span>
+                                                </div>
+                                                <div>
+                                                    <AiFillCar />
+                                                    <span>{garage}</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </Link>
-                        </>
-                    )
-                })
-            }
-        </div>
+                                </Link>
+                            </>
+                        )
+                    })
+                }
+            </div>
+        </>
     )
 }
 
--- pages/property/index.js
+++ pages/property/index.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ListingCard from "../../components/reusable/ListingCard";
 import { gql, ApolloClient, InMemoryCache } from "@apollo/client";
-const index = ({ data }) => {
-    return (
-        <div>
-            <div className="home__listing__container container">
-                <ListingCard data={data} />
-            </div>
-        </div>
-    );
+const index = ({ data, loading }) => {
+  return (
+    <div>
+      <div className="home__listing__container container">
+        <ListingCard data={data} loading={loading} />
+      </div>
+    </div>
+  );
 };
 
 export default index;
 
 export async function getStaticProps() {
-    const client = new ApolloClient({
-        uri: "http://localhost:1337/graphql",
-        cache: new InMemoryCache(),
-    });
-    const { loading, error, data } = await client.query({
-        query: gql`
+  const client = new ApolloClient({
+    uri: "http://localhost:1337/graphql",
+    cache: new InMemoryCache(),
+  });
+  const { loading, error, data } = await client.query({
+    query: gql`
       query {
         listings {
           data {
@@ -54,11 +54,11 @@ export async function getStaticProps() {
         }
       }
     `,
-    });
+  });
 
-    return {
-        props: {
-            data
-        },
-    };
+  return {
+    props: {
+      data
+    },
+  };
 }
