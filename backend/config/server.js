5eee203d5564d636ecd6b223c5c38f0fc5c96a66
Sajib Hossain <sajibhossain1185@gmail.com>
2022-06-28 03:51:47 +0600

    final push

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['GwfB3Xa4Mgm59NcO3TlX0w ==', 'A3nZ31Z + PxGQj346BvMjdg ==', 'o + yb / ynDQb5kkqtBBfRo9w ==', 'zJBu25EopkHzOIXlH9muNA ==']),
   },
 });
